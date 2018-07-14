3e8f5d061e67ac638b24da9e41f2f41c8077a5fa
Xiaochao Luo <zack0813@0813.com>
2018-07-14 15:03:43 -0400

    Temporary user login and register
    some temporary work for report package.

--- subby/urls.py
+++ subby/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from .views import application, user
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'subby'
 
@@ -9,6 +11,16 @@ urlpatterns = [
     path('', application.index, name='index'),
 
     # User views
-    path('users', user.index, name='user_index'),
-    path('users/<int:user_id>', user.show, name='user_show')
+    path('users/', user.index, name='user_index'),
+    path('users/<int:user_id>', user.show, name='user_show'),
+	
+	
+	################
+	##Temporary path for report prototype
+	################
+	path('login/', user.login, name='login'),
+	path('logout/', user.logout, name='logout'),
+	path('signup/', user.signup, name='signup'),
+	
 ]
+
