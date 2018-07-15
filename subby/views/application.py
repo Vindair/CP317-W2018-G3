1cf1127efbe4294c2c4f3f11721ce8a7aecd58ce
Alex Kirsopp <alex.kirsopp@vidyard.com>
2018-07-15 12:23:17 -0400

    base.html is edited (#11)
    Work by @Kdeer on storing static assets within `subby_project`

--- subby/views/application.py
+++ subby/views/application.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.conf import settings
 # Create your views here.
 
 def index(req):
-    return HttpResponse("Subby App Index Route")
+    return render(req, 'application/base.html')
