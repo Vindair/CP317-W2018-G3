d85cef736cbe144d380cab04a75b3e200f21f51b
Alex Kirsopp <alex.kirsopp@vidyard.com>
2018-07-21 00:37:19 -0400

    User admin (#22)
    * wip
    
    * Add admin link, basic admin authorization, and styled error messages
    
    * Whitespace
    
    * Add custom CSS block, basic search and style for users

--- subby/urls.py
+++ subby/urls.py
@@ -1,49 +1,32 @@
 from django.urls import path
-
-from .views import application, user, sublet, account
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import application, user, sublet, account
 
 
 app_name = 'subby'
 
 urlpatterns = [
-    # Base application view (i.e. index, about, contact...)
-    path('', application.index, name='index'),
+  # Base application view
+  path('', application.index, name='index'),
 
-    # User views
-    path('users/', user.index, name='user_index'),
-    path('users/<int:user_id>', user.show, name='user_show'),
-	
-	
-	################
-	##Temporary login/signup/logout path for report prototype
-	################
-	path('login/', user.login, name='login'),
-	path('logout/', user.logout, name='logout'),
-	path('signup/', user.signup, name='signup'),
-	
-	
-	################
-	##Temporary sublet path for report prototype
-	################
-	path('sublets/', sublet.SubletList.as_view(template_name='sublet/sublet_list.html'), name='SubletList'),
-	
-	path('sublets/<int:pk>/', sublet.SubletDetail.as_view(template_name='sublet/sublet_detail.html'), name='SubletDetail'),
-	
-	path('sublets/create_sublet', sublet.create_sublet, name='create_sublet'),
+  # User Admin views
+  path('users/', user.index, name='user_index'),
+  path('users/<int:user_id>', user.show, name='user_show'),
 
-	path('sublets/search', sublet.search, name='search'),
+  # User session views
+  path('login/', user.login, name='login'),
+  path('logout/', user.logout, name='logout'),
+  path('signup/', user.signup, name='signup'),
 
-	path('sublets/contact', user.contact_user, name='contact_user'),
-	
-	##############
-	##Temporary account path for report prototype
-	##############
-	path('account/', account.account_home, name='account_home'),
-	path('account/update', account.update_user_info, name='update_user_info'),
-	
-	
-	
+  # Account Paths
+  path('account/', account.account_home, name='account_home'),
+  path('account/update', account.update_user_info, name='update_user_info'),
+
+  # Sublet paths
+  path('sublets/', sublet.SubletList.as_view(template_name='sublet/sublet_list.html'), name='SubletList'),
+  path('sublets/<int:pk>/', sublet.SubletDetail.as_view(template_name='sublet/sublet_detail.html'), name='SubletDetail'),
+  path('sublets/create_sublet', sublet.create_sublet, name='create_sublet'),
+  path('sublets/search', sublet.search, name='search'),
+  path('sublets/contact', user.contact_user, name='contact_user'),
 ]
-
