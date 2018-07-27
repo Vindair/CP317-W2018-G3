f5120804231b89611aa050fc4617a05a1558a2cd
Vindair <39199957+Vindair@users.noreply.github.com>
2018-07-26 21:37:46 -0400

    Update urls.py

--- subby/urls.py
+++ subby/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
   
   # Rating paths
   path('ratings/', rating.list_user_rating, name='RatingList'),
-	path('ratings/write_review/', rating.write_review, name='write_review'),
-	path('ratings/update_review/', rating.update_review, name='update_review'),
+  path('ratings/write_review/', rating.write_review, name='write_review'),
+  path('ratings/update_review/', rating.update_review, name='update_review'),
 	
   # Favourite paths
   path('favourites/', favourite.FavouritesLister.index, name='favourite_list'),
