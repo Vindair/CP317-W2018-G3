22166a92b2c146ca49dab6151ea599699b39b1e8
Mackenzie Dang <mackenzie.dang@gmail.com>
2018-07-28 00:16:52 -0400

    Revert "Allow admin to edit ser information"
    This reverts commit ebe3e49ae5c506c494268af05f176c13b4e9b715.

--- subby/urls.py
+++ subby/urls.py
@@ -3,37 +3,37 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .views import application, user, sublet, account, rating
 
+
 app_name = 'subby'
 
 urlpatterns = [
-    # Base application view
-    path('', application.index, name='index'),
+  # Base application view
+  path('', application.index, name='index'),
 
-    # User Admin views
-    path('users/', user.index, name='user_index'),
-    path('users/<int:user_id>', user.show, name='user_show'),
-    path('users/<int:user_id>/update', user.edit, name='user_edit'),
+  # User Admin views
+  path('users/', user.index, name='user_index'),
+  path('users/<int:user_id>', user.show, name='user_show'),
+  path('users/<int:user_id>/update', user.edit, name='user_edit'),
 
-    # User session views
-    path('login/', user.login, name='login'),
-    path('logout/', user.logout, name='logout'),
-    path('signup/', user.signup, name='signup'),
+  # User session views
+  path('login/', user.login, name='login'),
+  path('logout/', user.logout, name='logout'),
+  path('signup/', user.signup, name='signup'),
 
-    # Account Paths
-    path('account/', account.account_home, name='account_home'),
-    path('account/update', account.update_user_info, name='update_user_info'),
+  # Account Paths
+  path('account/', account.account_home, name='account_home'),
+  path('account/update', account.update_user_info, name='update_user_info'),
 
-    # Sublet paths
-    path('sublets/', sublet.SubletList.as_view(template_name='sublet/sublet_list.html'), name='SubletList'),
-    path('sublets/<int:pk>/', sublet.SubletDetail.as_view(template_name='sublet/sublet_detail.html'),
-         name='SubletDetail'),
-    path('sublets/create_sublet', sublet.create_sublet, name='create_sublet'),
-    path('sublets/search', sublet.search, name='search'),
-    path('sublets/contact', user.contact_user, name='contact_user'),
-    path('sublets/update_sublet', sublet.update_sublet, name='update_sublet'),
-
-    # Rating paths
-    path('ratings/', rating.list_user_rating, name='RatingList'),
-    path('ratings/write_review/', rating.write_review, name='write_review'),
-    path('ratings/update_review/', rating.update_review, name='update_review'),
+  # Sublet paths
+  path('sublets/', sublet.SubletList.as_view(template_name='sublet/sublet_list.html'), name='SubletList'),
+  path('sublets/<int:pk>/', sublet.SubletDetail.as_view(template_name='sublet/sublet_detail.html'), name='SubletDetail'),
+  path('sublets/create_sublet', sublet.create_sublet, name='create_sublet'),
+  path('sublets/search', sublet.search, name='search'),
+  path('sublets/contact', user.contact_user, name='contact_user'),
+  path('sublets/update_sublet', sublet.update_sublet, name='update_sublet'),
+  
+  # Rating paths
+  path('ratings/', rating.list_user_rating, name='RatingList'),
+	path('ratings/write_review/', rating.write_review, name='write_review'),
+	path('ratings/update_review/', rating.update_review, name='update_review'),
 ]
--- subby/views/user.py
+++ subby/views/user.py
@@ -1,9 +1,15 @@
+import json
+import requests
+
 from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
 from subby.models import User
 from django.contrib import auth
 from django.contrib import messages
+
+from django.http import HttpResponse
 from django.db.models import Q
 from django.db import IntegrityError
+
 
 from django.contrib.auth import get_user_model
 
@@ -27,7 +33,6 @@ def __ensure_admin(func):
 # GET /users
 @__ensure_admin
 def index(req):
-
     query = req.GET.get('query', None)
 
     if query == None:
@@ -51,6 +56,7 @@ def show(req, user_id):
 
 
 def contact_user(request):
+<<<<<<< HEAD
     if request.method == 'POST':
         if request.POST['name'] and request.POST['email'] and request.POST['message']:
             user = User.objects.get(id=request.POST['posterid'])
@@ -109,6 +115,97 @@ def logout(request):
 
 @__ensure_admin
 def edit(request, user_id):
+=======
+	if request.method == 'POST':
+		recaptcha_response = request.POST.get('grecaptcha-token')
+		data = {
+			'secret': '6Ledc2YUAAAAAFYJYWjB2a8HZWXmtm4iFKyOJeio',
+			'response': recaptcha_response
+		}
+		r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+		result = r.json()
+
+		if result['score'] < 0.4:
+			messages.add_message(request, messages.INFO, 'Bot Verification Failed.')
+			return redirect('subby:SubletDetail', request.POST['subid'])
+		print(result['score'])
+
+		if request.POST['name'] and request.POST['email'] and request.POST['message']:
+			user = User.objects.get(id = request.POST['posterid'])
+			user.email_user(request.POST['name'] + 'for ' + request.POST['sublettitle'], request.POST['message'], request.POST['email'])
+			#return render(request, 'sublet/sublet_detail.html', {'success' : 'Thanks for leaving a message! You will be contacted shortly.'})
+			messages.add_message(request, messages.INFO, 'You have successfully left a message for this lister!')
+			return redirect('subby:SubletDetail', request.POST['subid'])
+		else:
+			messages.add_message(request, messages.INFO, 'All fields must be filled out when leaving a message.')
+			return redirect('subby:SubletDetail', request.POST['subid'])
+	else:
+		messages.add_message(request, messages.INFO, 'Something went wrong!')
+		return redirect('subby:SubletDetail', request.POST['subid'])
+
+
+def signup(request):
+	if request.method == 'POST':
+		recaptcha_response = request.POST.get('recaptcha-token')
+		data = {
+			'secret': '6Ledc2YUAAAAAFYJYWjB2a8HZWXmtm4iFKyOJeio',
+			'response': recaptcha_response
+		}
+		r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+		result = r.json()
+
+		if result['score'] < 0.4:
+			return render(request, 'application/base.html', {'signup_error': 'Bot verification failed'})
+
+		#User has info and wants an account now!
+		if not request.POST['username']:
+			return render(request, 'application/base.html', {'signup_error' : 'Please enter an email address'})
+		if request.POST['password'] == request.POST['password-confirm']:
+			try:
+				user = User.objects.get(email = request.POST['username'])
+				return render(request, 'application/base.html', {'signup_error':'Username has already been taken'})
+			except User.DoesNotExist:
+				user = User.objects.create_user(request.POST['username'], \
+				password = request.POST['password'])
+				auth.login(request, user)
+				return redirect('subby:index')
+		else:
+			return render(request, 'application/base.html', {'signup_error':'Passwords must match'})
+	elif request.method == 'GET':
+		p = request.GET.copy()
+		name = p['username']
+		if User.objects.filter(email=name):
+			return HttpResponse(False)
+		else:
+			return HttpResponse(True)
+	else:
+		return render(request, 'application/base.html')
+
+
+def login(request):
+	if request.method == 'POST':
+		user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			auth.login(request, user)
+			returned_render = redirect('subby:index')
+		else:
+			returned_render = render(request, 'application/base.html',{'login_error':'Invalid Email or Password Entered.'})
+	else:
+		returned_render = render(request, 'application/base.html')
+	return returned_render
+
+# POST /logout
+def logout(request):
+  if request.method == 'POST':
+    auth.logout(request)
+    return redirect('subby:index')
+
+@__ensure_admin
+def edit(request, user_id):
+    if req.user.is_admin == True:
+        messages.success(req,'admin')
+
+>>>>>>> 6339d4364576934cce9e17527a4e93200f8fec4d
     if request.method == 'POST':
         user = User.objects.get(id=user_id)
         try:
@@ -127,3 +224,7 @@ def edit(request, user_id):
     else:
         # User wants to enter info
         return render(request, 'application/base.html')
+<<<<<<< HEAD
+=======
+
+>>>>>>> 6339d4364576934cce9e17527a4e93200f8fec4d
