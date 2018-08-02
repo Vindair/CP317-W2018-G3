cca327361db1e2a30e743534e4c9538853c54915
Mackenzie Dang <mackenzie.dang@gmail.com>
2018-08-02 14:47:29 -0400

    Added client side validation for report modal, updated server side validation for report model to support messages

--- subby/views/report.py
+++ subby/views/report.py
@@ -3,6 +3,7 @@ from subby.models.report import Report
 from django.shortcuts import render, redirect, get_object_or_404
 from subby.decorators.loginrequiredmessage import message_login_required
 from subby.models.sublet import Sublet
+from django.contrib import messages
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -11,7 +12,7 @@ User = get_user_model()
 def create_report(request, user_id, sublet_id):
 	
 	if request.method == 'POST':
-		if request.POST['issue'] and request.POST['description']:
+		if request.POST.get('issue', None) and request.POST['description']:
 			user = User.objects.get(id=user_id)
 			sublet = Sublet.objects.get(id=sublet_id) 
 
@@ -20,8 +21,11 @@ def create_report(request, user_id, sublet_id):
 							issue=request.POST['issue'],
 							description=request.POST['description'],
 							)
-
+			messages.add_message(request, messages.SUCCESS, 'You have successfully left a report.')
 			return redirect('subby:SubletDetail',sublet_id)
+		else:
+			messages.add_message(request, messages.ERROR, 'All fields must be checked/filled in.')
+			return redirect('subby:SubletDetail', sublet_id)
 	
 	report_dict = {
 		'user_id':user_id,
