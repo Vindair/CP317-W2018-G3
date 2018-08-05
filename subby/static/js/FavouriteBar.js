325846bf563be42c23b861dfd4f19f9f14a48b4d
Vindair <39199957+Vindair@users.noreply.github.com>
2018-08-05 15:57:30 -0400

    js for opening animations of fav bar

--- subby/static/js/FavouriteBar.js
+++ subby/static/js/FavouriteBar.js
@@ -1,13 +1,21 @@
-/* Opening functions for favourite menu bar */
+/* Opening functions for favourite nav bar */
 function open_fav_menu() {
-	document.getElementById("favourite-menu-button").style.visibility = "hidden";
-    document.getElementById("favNav").style.width = "25%";
-    document.getElementById("main-page").style.marginLeft = "25%";
-}
 
-/* Closeing functions for favourite menu bar */
-function cancel_fav_menu() {
-	document.getElementById("favourite-menu-button").style.visibility = "visible";
-    document.getElementById("favNav").style.width = "0";
-    document.getElementById("main-page").style.marginLeft = "0";
+    var text = document.getElementById("favourite-menu-button").textContent
+
+    if(text == "Favourites"){
+    //open the favourites nav
+
+        document.getElementById("favourite-menu-button").textContent = "Close";
+        document.getElementById("favNav").style.width = "35%";
+        document.getElementById("main-page").style.marginLeft = "35%";
+
+    } else{
+    //close the favourites nav
+
+    	document.getElementById("favourite-menu-button").textContent = "Favourites";
+        document.getElementById("favNav").style.width = "0";
+        document.getElementById("main-page").style.marginLeft = "0";
+    }
+
 }
